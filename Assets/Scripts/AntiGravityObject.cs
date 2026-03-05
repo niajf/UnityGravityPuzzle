749948177f4346cd94750f033d8834dcbfaf0015
niajf <ufiha562@gmail.com>
2026-03-05 17:18:04 +0900

    プレイヤーと逆の重力で動くゴールを追加

---
+++ Assets/Scripts/AntiGravityObject.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class AntiGravityObject : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 targetUpVector = Vector3.down; // 重力の向き
+    private bool isReverse = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        GravityManager.Instance.OnGravityChanged += OnGravityChanged;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 dir = targetUpVector;
+        if (isReverse) dir *= -1;
+
+        rb.AddForce(dir * Physics.gravity.magnitude);
+    }
+
+    void OnGravityChanged(Vector3 newGravityDir)
+    {
+        targetUpVector = newGravityDir;
+    }
+
+    void OnDestroy()
+    {
+        if (GravityManager.Instance != null)
+        {
+            GravityManager.Instance.OnGravityChanged -= OnGravityChanged;
+        }
+    }
+
+    public void normalGravity()
+    {
+        isReverse = false;
+    }
+
+    public void revesalGravity()
+    {
+        isReverse = true;
+    }
+}
--- Assets/Scripts/GoalArea.cs
+++ Assets/Scripts/GoalArea.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class GoalArea : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
         // 侵入したのが「Player」タグを持つオブジェクトなら
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Enter Goal.");
             // ゲーム進行管理クラスにクリアを通知する
