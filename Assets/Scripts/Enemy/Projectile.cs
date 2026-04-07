fe2cfa6d89dae8302e79f257894351ed5cb47f9f
niajf <ufiha562@gmail.com>
2026-04-07 09:49:55 +0900

    refactor: プレイヤー意外と衝突した際も弾を返却するように変更

--- Assets/Scripts/Enemy/Projectile.cs
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -19,12 +19,27 @@ public class Projectile : MonoBehaviour
         if (pool == null) return;
         if (isReturned) return;
 
+        // 一定時間経過したら、オブジェクトをプールへ返却
         timer += Time.deltaTime;
         if (timer >= lifetime)
         {
-            isReturned = true;
-            pool.ReleaseObject(gameObject);
-            timer = 0f;
+            ReleaseObject(gameObject);
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // プレイヤー意外との衝突ならば、オブジェクトを返却
+        if (!other.CompareTag("Player"))
+        {
+            ReleaseObject(gameObject);
+        }
+    }
+
+    void ReleaseObject(GameObject ob)
+    {
+        isReturned = true;
+        timer = 0f;
+        pool.ReleaseObject(ob);
     }
 }
